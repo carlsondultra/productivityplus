c4804d0b8fed64d33cb0bab17620bca4a199bd37
carlsondultra <carlsondultra@users.noreply.github.com>
2023-11-22 20:20:21 -0500

    fixing url on organization not changing

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
                     hidePersonal
                     afterCreateOrganizationUrl="/organization/:id"
                     afterLeaveOrganizationUrl="/select-org"
-                    afterSelectOrganizationUrl="organization/:id"
+                    afterSelectOrganizationUrl="/organization/:id"
                     appearance={{
                         elements: {
                             rootBox: {
