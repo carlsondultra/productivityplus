0d4f8941527990fd15e327c72cd3b6b00cc81078
carlsondultra <carlsondultra@users.noreply.github.com>
2023-11-30 11:44:20 -0500

    adding delete board functionality

--- app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,3 +1,4 @@
+import { deleteBoard } from "@/actions/delete-board";
 import { Button } from "@/components/ui/button";
 
 interface BoardProps {
@@ -9,12 +10,13 @@ export const Board = ({
     title,
     id
 }: BoardProps) => {
+    const deleteBoardWithId = deleteBoard.bind(null, id)
     return (
-        <form className="flex items-center gap-x-2">
+        <form action={deleteBoardWithId} className="flex items-center gap-x-2">
             <p>
                 Board title: {title}
             </p>
-            <Button variant="destructive" size="sm">
+            <Button type="submit" variant="destructive" size="sm">
                 Delete
             </Button>
         </form>
