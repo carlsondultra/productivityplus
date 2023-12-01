12f96d55c8b0601b793776fe394e1e76275951e8
carlsondultra <carlsondultra@users.noreply.github.com>
2023-11-30 22:54:42 -0500

    changing form-input into a component

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -3,6 +3,7 @@
 import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
 import { useFormState } from "react-dom";
+import { FormInput } from "./form-input";
 
 export const Form = () => {
     const initialState = { message: null, errors: {} }
@@ -19,15 +20,7 @@ export const Form = () => {
                     placeholder="Enter a board title"
                     className="border-black border p-1"
                 />
-                {state?.errors?.title ? (
-                    <div>
-                        {state.errors.title.map((error: string) => (
-                            <p key={error} className="text-rose-500">
-                                {error}
-                            </p>
-                        ))}
-                    </div>
-                ) : null}
+                <FormInput errors={state?.errors}/>
             </div>
             <Button type="submit">
                 Submit
