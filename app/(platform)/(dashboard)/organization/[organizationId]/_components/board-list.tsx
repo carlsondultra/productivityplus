b8f3ac619309e76c106b50f31848e6a8953fd1f2
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-07 22:21:52 -0500

    adding hint component to board

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,10 +1,11 @@
-import { User2 } from "lucide-react"
+import { Hint } from "@/components/hint"
+import { HelpCircle, User2 } from "lucide-react"
 
 export const BoardList = () => {
     return (
         <div className="space-y-4">
             <div className="flex items-center font-semibold text-lg text-neutral-700">
-                <User2 className="h-6 w-6 mr-2"/>
+                <User2 className="h-6 w-6 mr-2" />
                 Your Boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -17,6 +18,16 @@ export const BoardList = () => {
                     <span className="text-xs">
                         5 remaining
                     </span>
+                    <Hint
+                        sideOffset={40}
+                        description={`
+                        Free Workspaces can have up to 5 open boards. For unlimited boards, upgrade this workspace.
+                        `}
+                    >
+                        <HelpCircle
+                            className="absolute bottom-2 right-2 h-[14px] w-[14px]"
+                        />
+                    </Hint>
                 </div>
             </div>
         </div>
