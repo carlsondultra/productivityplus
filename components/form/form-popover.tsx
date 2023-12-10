76dfd798904773e11384fbd155aed752c6dca4fb
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-09 22:48:01 -0500

    adding form portion to popover component

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -14,6 +14,7 @@ import { FormInput } from "./form-input"
 import { FormSubmit } from "./form-submit"
 import { Button } from "../ui/button"
 import { X } from "lucide-react"
+import { toast } from "sonner"
 
 interface FormPopoverProps {
     children: React.ReactNode;
@@ -28,6 +29,23 @@ export const FormPopover = ({
     align,
     sideOffset = 0,
 }: FormPopoverProps) => {
+    const { execute, fieldErrors } = useAction(createBoard, {
+        onSuccess: (data) => {
+            console.log({ data })
+            toast.success("Board Created!")
+        },
+        onError: (error) => {
+            console.log({ error })
+            toast.error(error)
+        }
+    })
+
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string
+
+        execute({ title })
+    }
+
     return (
         <Popover>
             <PopoverTrigger asChild>
@@ -48,9 +66,22 @@ export const FormPopover = ({
                         className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
                         variant="ghost"
                     >
-                        <X className="h-4 w-4"/>
+                        <X className="h-4 w-4" />
                     </Button>
                 </PopoverClose>
+                <form action={onSubmit} className="space-y-4">
+                    <div className="space-y-4">
+                        <FormInput
+                            id="title"
+                            label="board title"
+                            type="text"
+                            errors={fieldErrors}
+                        />
+                    </div>
+                    <FormSubmit className="w-full">
+                        Create
+                    </FormSubmit>
+                </form>
             </PopoverContent>
         </Popover>
     )
