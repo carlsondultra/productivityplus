d5003509e0f4182db9884f8929ccf0ea7df41051
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-09 21:12:13 -0500

    adding close button to popover

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -2,6 +2,7 @@
 
 import {
     Popover,
+    PopoverClose,
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
@@ -11,6 +12,8 @@ import { createBoard } from "@/actions/create-board"
 
 import { FormInput } from "./form-input"
 import { FormSubmit } from "./form-submit"
+import { Button } from "../ui/button"
+import { X } from "lucide-react"
 
 interface FormPopoverProps {
     children: React.ReactNode;
@@ -39,6 +42,15 @@ export const FormPopover = ({
                 <div className="text-sm font-medium text-center text-neutral-600 pb-4">
                     Create board
                 </div>
+                {/* //close button */}
+                <PopoverClose asChild>
+                    <Button
+                        className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+                        variant="ghost"
+                    >
+                        <X className="h-4 w-4"/>
+                    </Button>
+                </PopoverClose>
             </PopoverContent>
         </Popover>
     )
