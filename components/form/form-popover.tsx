e4b05ee6fd4c3182002e214895daa68f1899fe68
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-12 14:41:52 -0500

    routing user to board once created

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -17,6 +17,7 @@ import { X } from "lucide-react"
 import { toast } from "sonner"
 import { FormPicker } from "./form-picker"
 import { ElementRef, useRef } from "react"
+import { useRouter } from "next/navigation"
 
 interface FormPopoverProps {
     children: React.ReactNode;
@@ -31,12 +32,14 @@ export const FormPopover = ({
     align,
     sideOffset = 0,
 }: FormPopoverProps) => {
+    const router = useRouter()
     const closeRef = useRef<ElementRef<"button">>(null);
 
     const { execute, fieldErrors } = useAction(createBoard, {
         onSuccess: (data) => {
             toast.success("Board Created!")
             closeRef.current?.click();
+            router.push(`/board/${data.id}`)
         },
         onError: (error) => {
             toast.error(error)
