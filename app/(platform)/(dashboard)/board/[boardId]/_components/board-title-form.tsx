2088c8b072381fdea449dd410d76932613494c71
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-14 19:20:15 -0500

    adding transparency effect to button

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -12,6 +12,7 @@ export const BoardTitleForm = ({
 }: BoardTitleFormProps) => {
     return (
         <Button
+            variant="transparent"
             className="font-bold text-lg h-auto w-auto p-1 px-2"
         >
             {data.title}
