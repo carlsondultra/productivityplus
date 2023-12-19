ae603b587bc5e4d524c518b162af4f2127ce5bbd
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-18 20:17:03 -0500

    adding buttons to list component form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,12 +1,17 @@
 "use client"
 
-import { Plus } from "lucide-react"
+import { Plus, X } from "lucide-react"
 import { ListWrapper } from "./list-wrapper"
 import { useState, useRef, ElementRef } from "react"
 import { useEventListener, useOnClickOutside } from "usehooks-ts"
 import { FormInput } from "@/components/form/form-input"
+import { useParams } from "next/navigation"
+import { FormSubmit } from "@/components/form/form-submit"
+import { Button } from "@/components/ui/button"
 
 export const ListForm = () => {
+    const params = useParams()
+
     const [isEditing, setIsEditing] = useState(false);
 
     const formRef = useRef<ElementRef<"form">>(null)
@@ -32,7 +37,7 @@ export const ListForm = () => {
     useEventListener("keydown", onKeyDown);
     useOnClickOutside(formRef, disableEditing)
 
-    if(isEditing) {
+    if (isEditing) {
         return (
             <ListWrapper>
                 <form
@@ -45,7 +50,23 @@ export const ListForm = () => {
                         className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
                         placeholder="Enter list title..."
                     />
-
+                    <input
+                        hidden
+                        value={params.boardId}
+                        name="boardId"
+                    />
+                    <div className="flex items-center gap-x-1">
+                        <FormSubmit>
+                            Add List
+                        </FormSubmit>
+                        <Button
+                            onClick={disableEditing}
+                            size="sm"
+                            variant="ghost"
+                        >
+                            <X className="h-5 w-5" />
+                        </Button>
+                    </div>
                 </form>
             </ListWrapper>
         )
@@ -54,10 +75,10 @@ export const ListForm = () => {
     return (
         <ListWrapper>
             <button
-            onClick={enableEditing}
+                onClick={enableEditing}
                 className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm"
             >
-                <Plus className="h-4 w-4 mr-2"/>
+                <Plus className="h-4 w-4 mr-2" />
                 Add a list
             </button>
         </ListWrapper>
