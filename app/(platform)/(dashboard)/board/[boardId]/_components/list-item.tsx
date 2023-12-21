3f2784ea66843f4ca98d463b85e5e8095596bb5c
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-20 22:02:56 -0500

    added ability to display list titles on frontend once created

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -15,7 +15,7 @@ export const ListItem = ({
     return (
         <li className="shrink-0 h-full w-[272px] select-none">
             <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-                <ListHeader />
+                <ListHeader data={data} />
             </div>
         </li>
     )
