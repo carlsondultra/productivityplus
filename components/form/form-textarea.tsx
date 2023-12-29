4d927d0f4f765ebdd45811bdeaa7afc7836f7d7b
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-29 11:55:30 -0500

    adding optional on click in textarea

--- components/form/form-textarea.tsx
+++ components/form/form-textarea.tsx
@@ -16,7 +16,7 @@ interface FormTextareaProps {
     errors?: Record<string, string[] | undefined>;
     className?: string;
     onBlur?: () => void;
-    onClick: () => void;
+    onClick?: () => void;
     onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
     defaultValue?: string
 }
