b1a1bd773d25f432e61dd7dc6d601945a9ca5edf
carlsondultra <carlsondultra@users.noreply.github.com>
2023-12-30 14:42:01 -0500

    displaying text area once editing card

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { FormSubmit } from "@/components/form/form-submit";
+import { FormTextarea } from "@/components/form/form-textarea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { forwardRef } from "react";
 
 interface CardFormProps {
@@ -11,12 +13,40 @@ interface CardFormProps {
     isEditing: boolean
 }
 
-export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps> (({
+export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
     listId,
     enableEditing,
     disableEditing,
     isEditing,
 }, ref) => {
+    if (isEditing) {
+        return (
+            <form
+                className="m-1 py-0.5 px-1 space-y-4"
+            >
+                <FormTextarea 
+                    id="title"
+                    onKeyDown={() => {}}
+                    ref={ref}
+                    placeholder="Enter a title"
+                />
+                <input
+                    hidden
+                    id="listId"
+                    name="listId"
+                    value={listId}
+                />
+                <div className="flex items-center gap-x-1">
+                    <FormSubmit>
+                        Add Card
+                    </FormSubmit>
+                    <Button onClick={disableEditing} size="sm" variant="ghost">
+                        <X className="h-5 w-5"/>
+                    </Button>
+                </div>
+            </form>
+        )
+    }
     return (
         <div className="pt-2 px-2">
             <Button
