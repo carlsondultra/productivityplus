b4ae84b487b9566d4fa323f8d7b3a75a92935a42
carlsondultra <carlsondultra@users.noreply.github.com>
2024-01-10 19:08:16 -0500

    displaying card title upon opening modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -2,6 +2,10 @@
 
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 import { useCardModal } from "@/hooks/use-card-modal"
+import { fetcher } from "@/lib/fetcher"
+import { CardWithList } from "@/types"
+
+import { useQuery } from "@tanstack/react-query"
 
 export const CardModal = () => {
 
@@ -9,13 +13,18 @@ export const CardModal = () => {
     const isOpen = useCardModal((state) => state.isOpen);
     const onClose = useCardModal((state) => state.onClose)
 
+    const { data: cardData } = useQuery<CardWithList>({
+        queryKey: ["card", id],
+        queryFn: () => fetcher(`/api/cards/${id}`),
+    })
+
     return (
         <Dialog
             open={isOpen}
             onOpenChange={onClose}
         >
             <DialogContent>
-                I am a modal
+                {cardData?.title}
             </DialogContent>
         </Dialog>
     )
