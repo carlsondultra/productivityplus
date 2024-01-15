a68d13902d5ddec10f104f52be73aadb063d7254
carlsondultra <carlsondultra@users.noreply.github.com>
2024-01-14 21:54:02 -0500

    Adding basic design for editing of description

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,8 +1,15 @@
 "use client"
 
+import { FormSubmit } from "@/components/form/form-submit"
+import { FormTextarea } from "@/components/form/form-textarea"
+import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
 import { CardWithList } from "@/types"
+import { useQueryClient } from "@tanstack/react-query"
 import { AlignLeft } from "lucide-react"
+import { useParams } from "next/navigation"
+import { ElementRef, useRef, useState } from "react"
+import { useEventListener, useOnClickOutside } from "usehooks-ts"
 
 interface DescriptionProps {
     data: CardWithList
@@ -11,6 +18,39 @@ interface DescriptionProps {
 export const Description = ({
     data
 }: DescriptionProps) => {
+    const queryClient = useQueryClient();
+    const params = useParams();
+
+    const [isEditing, setIsEditing] = useState(false)
+
+    const textareaRef = useRef<ElementRef<"form">>(null)
+    const formRef = useRef<ElementRef<"form">>(null)
+
+    const enableEditing = () => {
+        setIsEditing(true)
+        setTimeout(() => {
+            textareaRef.current?.focus()
+        })
+    }
+
+    const disableEditing = () => {
+        setIsEditing(false)
+    }
+
+    const onKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+            disableEditing()
+        }
+    }
+
+    useEventListener("keydown", onKeyDown)
+    useOnClickOutside(formRef, disableEditing)
+
+    const onSubmit = (formData: FormData) => {
+        const description = formData.get("description") as string;
+        const boardId = params.boardId as string;
+    }
+
     return (
         <div className="flex items-start gap-x-3 w-full">
             <AlignLeft className="h-5 w-5 mt-0.5 text-neutral-700" />
@@ -18,8 +58,41 @@ export const Description = ({
                 <p className="font-semibold text-neutral-700 mb-2">
                     Description
                 </p>
+                {isEditing ? (
+                    <form
+                        ref={formRef}
+                        className="space-y-2"
+                    >
+                        <FormTextarea 
+                            id="description"
+                            className="w-full mt-2"
+                            placeholder="Add a more detailed description"
+                            defaultValue={data.description || undefined}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <FormSubmit>
+                                Save
+                            </FormSubmit>
+                            <Button
+                                type="button"
+                                onClick={disableEditing}
+                                size="sm"
+                                variant="ghost"
+                            >
+                                Cancel
+                            </Button>
+                        </div>
+                    </form>
+                ) : ( 
+                <div
+                onClick={enableEditing}
+                    role="button"
+                    className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
+                >
+                    {data.description || "Add a description..."}
+                </div>
+                )}
             </div>
-            {data.description}
         </div>
     )
 }
