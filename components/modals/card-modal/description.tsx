1ae3dc0915e825b46a79a34261a4a827dfaadd19
carlsondultra <carlsondultra@users.noreply.github.com>
2024-01-15 20:20:52 -0500

    disabling editing once description has been modified

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -55,6 +55,7 @@ export const Description = ({
                 queryKey: ["card", data.id],
             })
             toast.success(`Card "${data.title}" updated`)
+            disableEditing()
         },
         onError: (error) => {
             toast.error(error)
