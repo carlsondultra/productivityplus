86830bb889932d61939c0b33387fe44d0a74a422
carlsondultra <carlsondultra@users.noreply.github.com>
2024-01-28 22:55:34 -0500

    redirecting user to stripe upon clicking upgrade button

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -4,9 +4,25 @@ import { Dialog, DialogContent } from "@/components/ui/dialog"
 import { useProModal } from "@/hooks/use-pro-modal";
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { useAction } from "@/hooks/use-action";
+import { stripeRedirect } from "@/actions/stripe-redirect";
+import { toast } from "sonner";
 
 export const ProModal = () => {
     const proModal = useProModal();
+
+    const { execute, isLoading } = useAction(stripeRedirect, {
+        onSuccess: (data) => {
+            window.location.href = data
+        },
+        onError: (error) => {
+            toast.error(error)
+        }
+    })
+
+    const onClick = () => {
+        execute({})
+    }
 
     return (
         <Dialog
@@ -15,7 +31,7 @@ export const ProModal = () => {
         >
             <DialogContent className="max-w-md p-0 overflow-hidden">
                 <div className="aspect-video relative flex items-center justify-center">
-                    <Image 
+                    <Image
                         src="/hero.svg"
                         alt="Hero"
                         className="object-cover"
@@ -38,6 +54,8 @@ export const ProModal = () => {
                         </ul>
                     </div>
                     <Button
+                        disabled={isLoading}
+                        onClick={onClick}
                         className="w-full"
                         variant="primary"
                     >
