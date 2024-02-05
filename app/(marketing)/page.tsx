d0ced51a19dfd2449923b105560aebdea1aa200b
carlsondultra <carlsondultra@users.noreply.github.com>
2024-02-04 20:00:15 -0500

    editing landing page to include image of dashboard

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -30,7 +30,7 @@ const MarketingPage = () => {
             <div className={cn(
                 "flex items-center justify-center flex-col",
                 headingFont.className,
-                )}>
+            )}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2" />
                     Task Management made simple!
@@ -38,19 +38,19 @@ const MarketingPage = () => {
                 <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
                     Add ProductivityPlus to your workflow!
                 </h1>
-                <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-                    raise your value today.
-                </div>
+
+                <img src="dashboard.png" alt="dashboard" width="80%" height="80%"/>
+
             </div>
             <div className={cn(
-                "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                "text-sm md:text-xl text-neutral-500 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
                 textFont.className,
-                )}>
-                Collaborate, manage projects, and reach new heights.
+            )}>
+                Work together with colleagues, oversee projects, and achieve success!
             </div>
             <Button className="mt-6" size="lg" asChild>
                 <Link href="/sign-up">
-                    Try out productivityplus!
+                    Try out productivityplus now!
                 </Link>
             </Button>
         </div>
